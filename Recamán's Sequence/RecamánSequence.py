0e3c28c6b0bbc2b5f76b50c4fe989f7a36689b77
VCZLee <31873761+VCZLee@users.noreply.github.com>
2018-06-17 06:02:39 -0400

    Added spiral visualization of the paths the sequence takes through the integers using the turtle library

--- Recamán's Sequence/RecamánSequence.py
+++ Recamán's Sequence/RecamánSequence.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
+import turtle
+
 
 starting_point = 0
 starting_addend = 1
-num_iterations = 500000
+num_iterations = 1000
 recaman_set = set()
 recaman_list = []
 recaman_set.add(starting_point)
@@ -16,5 +18,30 @@ for i in range(1, num_iterations):
     starting_addend = starting_addend + 1
     recaman_set.add(starting_point)
     recaman_list.append(starting_point)
-plt.scatter(range(1,num_iterations + 1), recaman_list)
-plt.show()
+
+# print(recaman_list)
+#
+# plt.scatter(range(1,num_iterations + 1), recaman_list)
+# plt.show()
+
+
+
+integer_scale = 2
+sizing = max(recaman_list) * integer_scale
+
+screen = turtle.Screen()
+screen.screensize(10000, 10000)
+
+t = turtle.Pen()
+t.speed(0)
+t.hideturtle()
+
+t.right(90)
+for i in range(0, num_iterations - 1):
+    factor = integer_scale * (i + 1)
+    if i % 2 == 1:
+        factor = factor * -1
+    if recaman_list[i+1] - recaman_list[i] < 0:
+        factor = factor * -1
+    t.circle(factor, 180)
+turtle.done()
