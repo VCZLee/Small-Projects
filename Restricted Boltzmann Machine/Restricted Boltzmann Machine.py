01f2500047fb542b74245f858701b79e07e12b9a
VCZLee <31873761+VCZLee@users.noreply.github.com>
2018-07-08 01:23:11 -0400

    added new methods and attributes to RBM class

--- Restricted Boltzmann Machine/Restricted Boltzmann Machine.py
+++ Restricted Boltzmann Machine/Restricted Boltzmann Machine.py
@@ -1,13 +1,63 @@
 import numpy as np
 
-# num_visible = 6
-# num_hidden = 3
-# learning_rate = 0.1
-# batch_size = 7
-# cd_iterations = 1
-# num_epochs = 2500
+class RestrictedBoltzmannMachine:
+    def __init__(self, num_hidden):
+        self.weights = None
+        self.forward_bias = None
+        self.backward_bias = None
+        self.num_hidden = num_hidden
+        self.num_visible = None
+        self.weights = None
+        self.forward_bias = None
+        self.backward_bias = None
 
-num_weights = num_visible*num_hidden
+    def fit(self, x, learning_rate, batch_size, cd_iterations, epochs):
+        if self.num_visible != x.shape[1]:
+            self.num_visible = x.shape[1]
+            self.weights = np.random.randn(self.num_visible, self.num_hidden)
+            self.forward_bias = np.random.randn(1, self.num_hidden)
+            self.backward_bias = np.random.randn(1, self.num_visible)
+        for epoch in range(epochs):
+            random_rows = np.random.choice(x.shape[0], batch_size, replace=False)
+            train = x[random_rows, :]
+            contrastive_divergence_list = []
+            forward_bias_list = []
+            backward_bias_list = []
+            for row in range(train.shape[0]):
+                visible = train[row, :]
+                hidden_activation = visible.dot(self.weights) + self.forward_bias
+                hidden_prob = 1 / (1 + np.exp(-hidden_activation))
+                hidden = (np.random.uniform(size=self.num_hidden) < hidden_prob).astype(int)
+                for iteration in range(cd_iterations):
+                    recon_activation = hidden.dot(np.transpose(self.weights)) + self.backward_bias
+                    recon_prob = 1 / (1 + np.exp(-recon_activation))
+                    recon = (np.random.uniform(size=self.num_visible) < recon_prob).astype(int)
+                    recon_hidden_activation = recon_prob.dot(self.weights) + self.forward_bias
+                    recon_hidden_prob = 1 / (1 + np.exp(-recon_hidden_activation))
+                    recon_hidden = (np.random.uniform(size=self.num_hidden) < recon_hidden_prob).astype(int)
+                    if iteration < cd_iterations - 1:
+                        hidden = recon_hidden
+                contrastive_divergence_list.append(np.outer(visible, hidden) - np.outer(recon, recon_hidden))
+                forward_bias_list.append(hidden - recon_hidden)
+                backward_bias_list.append(visible - recon)
+            self.weights = self.weights + learning_rate * np.mean(contrastive_divergence_list, axis=0)
+            self.forward_bias = self.forward_bias + learning_rate * np.mean(forward_bias_list, axis=0)
+            self.backward_bias = self.backward_bias + learning_rate * np.mean(backward_bias_list, axis=0)
+        return self
+
+    def reconstruct(self,x):
+        output = np.empty((0, x.shape[1])).astype(int)
+        for row in range(x.shape[0]):
+            visible = x[row, :]
+            hidden_activation = visible.dot(self.weights) + self.forward_bias
+            hidden_prob = 1 / (1 + np.exp(-hidden_activation))
+            hidden = (np.random.uniform(size=self.num_hidden) < hidden_prob).astype(int)
+            recon_activation = hidden.dot(np.transpose(self.weights)) + self.backward_bias
+            recon_prob = 1 / (1 + np.exp(-recon_activation))
+            recon = (np.random.uniform(size=self.num_visible) < recon_prob).astype(int)
+            output = np.append(output, recon, axis=0)
+        return output
+
 
 data = np.array([[1, 1, 1, 0, 0, 0],
                  [1, 0, 1, 0, 0, 0],
@@ -17,114 +67,6 @@ data = np.array([[1, 1, 1, 0, 0, 0],
                  [0, 0, 1, 1, 1, 0],
                  [1, 1, 1, 0, 0, 1]])
 
-weights = np.random.randn(num_visible, num_hidden)
-forward_bias = np.random.randn(1, num_hidden)
-backward_bias = np.random.randn(1, num_visible)
-
-
-for epoch in range(num_epochs):
-    random_rows = np.random.choice(data.shape[0], batch_size, replace=False)
-    train = data[random_rows, :]
-    contrastive_divergence_list = []
-    forward_bias_list = []
-    backward_bias_list = []
-    for row in range(train.shape[0]):
-        visible = train[row,:]
-        hidden_activation = visible.dot(weights) + forward_bias
-        hidden_prob = 1 / (1 + np.exp(-hidden_activation))
-        hidden = (np.random.uniform(size=num_hidden) < hidden_prob).astype(int)
-
-        for iteration in range(cd_iterations):
-            recon_activation = hidden.dot(np.transpose(weights)) + backward_bias
-            recon_prob = 1/(1+np.exp(-recon_activation))
-            recon = (np.random.uniform(size=num_visible) < recon_prob).astype(int)
-
-            recon_hidden_activation = recon_prob.dot(weights) + forward_bias
-            recon_hidden_prob = 1/(1+np.exp(-recon_hidden_activation))
-            recon_hidden = (np.random.uniform(size=num_hidden) < recon_hidden_prob).astype(int)
-            if iteration < cd_iterations - 1:
-                hidden = recon_hidden
-            contrastive_divergence_list.append(np.outer(visible, hidden) - np.outer(recon, recon_hidden))
-            forward_bias_list.append(hidden - recon_hidden)
-            backward_bias_list.append(visible - recon)
-    weights = weights + learning_rate*np.mean(contrastive_divergence_list, axis=0)
-    forward_bias = forward_bias + learning_rate*np.mean(forward_bias_list, axis=0)
-    backward_bias = backward_bias + learning_rate*np.mean(backward_bias_list, axis=0)
-
-output = np.empty((0,data.shape[1])).astype(int)
-
-for row in range(data.shape[0]):
-    visible = data[row,:]
-    hidden_activation = visible.dot(weights) + forward_bias
-    hidden_prob = 1 / (1 + np.exp(-hidden_activation))
-    hidden = (np.random.uniform(size=num_hidden) < hidden_prob).astype(int)
-    recon_activation = hidden.dot(np.transpose(weights)) + backward_bias
-    recon_prob = 1/(1+np.exp(-recon_activation))
-    recon = (np.random.uniform(size=num_visible) < recon_prob).astype(int)
-    output = np.append(output, recon, axis=0)
-
-print(output-data)
-
-
-class RestrictedBoltzmannMachine:
-    def __init__(self, num_hidden):
-        self.weights = None
-        self.forward_bias = None
-        self.backward_bias = None
-        self.num_hidden = num_hidden
-        self.num_visible = None
-
-    def fit(self, x, learning_rate, batch_size, cd_iterations, epochs):
-        self.num_visible = x.shape[1]
-        self.weights = np.random.randn(self.num_visible, self.num_hidden)
-        self.forward_bias = np.random.randn(1, self.num_hidden)
-        self.backward_bias = np.random.randn(1, self.num_visible)
-        for epoch in range(self.num_epochs):
-            random_rows = np.random.choice(x.shape[0], self.batch_size, replace=False)
-            train = x[random_rows, :]
-            contrastive_divergence_list = []
-            forward_bias_list = []
-            backward_bias_list = []
-
-
-
-
-
-
-
-# weights = weights + learning_rate*np.mean(contrastive_divergence_list, axis=0)
-# forward_bias = forward_bias + learning_rate*np.mean(forward_bias_list, axis=0)
-# backward_bias = backward_bias + learning_rate*np.mean(backward_bias_list, axis=0)
-
-
-# print(np.mean([a,b], axis=0))
-
-# num_visible = 6
-# num_hidden = 2
-# learning_rate = 0.1
-#
-# num_weights = num_visible*num_hidden
-#
-# visible = np.random.randint(2, size=(1, num_visible))
-# weights = np.random.randn(num_visible, num_hidden)
-# forward_bias = np.random.randn(1, num_hidden)
-#
-# hidden_activation = visible.dot(weights) + forward_bias
-# hidden_prob = 1/(1+np.exp(-hidden_activation))
-# hidden = (np.random.uniform(size=num_hidden) < hidden_prob).astype(int)
-# backward_bias = np.random.randn(1, num_visible)
-#
-# for epoch in range(1000):
-#     recon_activation = hidden.dot(np.transpose(weights)) + backward_bias
-#     recon_prob = 1/(1+np.exp(-recon_activation))
-#     recon = (np.random.uniform(size=num_visible) < recon_prob).astype(int)
-#
-#     recon_hidden_activation = recon_prob.dot(weights) + forward_bias
-#     recon_hidden_prob = 1/(1+np.exp(-recon_hidden_activation))
-#     recon_hidden = (np.random.uniform(size=num_hidden) < recon_hidden_prob).astype(int)
-#     hidden = recon_hidden
-#
-#     contrastive_divergence = np.outer(visible, hidden) - np.outer(recon, recon_hidden)
-#     weights = weights + learning_rate*contrastive_divergence
-#     forward_bias =  forward_bias + learning_rate*(hidden - recon_hidden)
-#     backward_bias = backward_bias + learning_rate*(visible - recon)
+rbm = RestrictedBoltzmannMachine(num_hidden=4)
+rbm = rbm.fit(data, 0.1, 3, 1, 10000)
+print(rbm.reconstruct(data))
