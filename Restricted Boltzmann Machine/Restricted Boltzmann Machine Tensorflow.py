2e23e7d40b74f03346d0fda1f233692e64e98ab9
vczlee <vczlee@gmail.com>
2018-07-30 13:31:50 -0400

    added more tensorflow code for RBM

--- Restricted Boltzmann Machine/Restricted Boltzmann Machine Tensorflow.py
+++ Restricted Boltzmann Machine/Restricted Boltzmann Machine Tensorflow.py
@@ -1,18 +1,18 @@
 import numpy as np
 import tensorflow as tf
 
-# x = np.array([[1, 1, 1, 0, 1],
-#               [1, 1, 1, 0, 0],
-#               [0, 0, 0, 1, 1],
-#               [0, 0, 0, 0, 1],
-#               [1, 1, 1, 1, 0],
-#               [0, 0, 0, 1, 0]])
+x = np.array([[1, 1, 1, 0, 1],
+              [1, 1, 1, 0, 0],
+              [0, 0, 0, 1, 1],
+              [0, 0, 0, 0, 1],
+              [1, 1, 1, 1, 0],
+              [0, 0, 0, 1, 0]])
 
-x = np.array([[1, 1, 1, 0, 1]])
+# x = np.array([[1, 1, 1, 0, 1]])
 
 num_visible = x.shape[1]
-num_hidden = 3
-use_sampling = False
+num_hidden = 2
+use_sampling = True
 
 with tf.Session() as sess:
     weights = tf.random_normal((num_visible, num_hidden))
@@ -21,4 +21,9 @@ with tf.Session() as sess:
     visible = tf.placeholder(tf.float32, [None, num_visible])
     hidden_activation = tf.add(tf.matmul(visible, weights),forward_bias)
     hidden_prob = tf.divide(1.0, tf.add(1.0, tf.exp(tf.negative(hidden_activation))))
-    print(sess.run(hidden_prob, {visible: x}))
+    if use_sampling:
+        hidden = tf.to_float(tf.less(tf.random_uniform((1, num_hidden)), hidden_prob))
+    else:
+        hidden = hidden_prob
+    recon_activation = tf.add(tf.matmul(hidden, weights, transpose_b=True),backward_bias)
+    print(sess.run(recon_activation, {visible:x}))
