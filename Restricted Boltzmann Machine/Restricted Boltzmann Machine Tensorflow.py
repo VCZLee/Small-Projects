2761849c75d44f69717d1ab9c89d5bcaeb5eef65
vczlee <vczlee@gmail.com>
2018-07-30 15:01:31 -0400

    added more tensorflow code for RBM

--- Restricted Boltzmann Machine/Restricted Boltzmann Machine Tensorflow.py
+++ Restricted Boltzmann Machine/Restricted Boltzmann Machine Tensorflow.py
@@ -8,10 +8,10 @@ x = np.array([[1, 1, 1, 0, 1],
               [1, 1, 1, 1, 0],
               [0, 0, 0, 1, 0]])
 
-# x = np.array([[1, 1, 1, 0, 1]])
+x = np.array([[1, 1, 1, 0, 1]])
 
 num_visible = x.shape[1]
-num_hidden = 2
+num_hidden = 3
 use_sampling = True
 
 with tf.Session() as sess:
@@ -19,11 +19,24 @@ with tf.Session() as sess:
     forward_bias = tf.random_normal((1, num_hidden))
     backward_bias = tf.random_normal((1, num_visible))
     visible = tf.placeholder(tf.float32, [None, num_visible])
-    hidden_activation = tf.add(tf.matmul(visible, weights),forward_bias)
+    hidden_activation = tf.add(tf.matmul(visible, weights), forward_bias)
     hidden_prob = tf.divide(1.0, tf.add(1.0, tf.exp(tf.negative(hidden_activation))))
     if use_sampling:
         hidden = tf.to_float(tf.less(tf.random_uniform((1, num_hidden)), hidden_prob))
     else:
         hidden = hidden_prob
-    recon_activation = tf.add(tf.matmul(hidden, weights, transpose_b=True),backward_bias)
-    print(sess.run(recon_activation, {visible:x}))
+    print(sess.run(hidden, {visible: x}))
+    recon_activation = tf.add(tf.matmul(hidden, weights, transpose_b=True), backward_bias)
+    recon_prob = tf.divide(1.0, tf.add(1.0, tf.exp(tf.negative(recon_activation))))
+    if use_sampling:
+        recon = tf.to_float(tf.less(tf.random_uniform((1, num_visible)), recon_prob))
+    else:
+        recon = recon_prob
+    print(sess.run(recon, {visible:x}))
+    recon_hidden_activation = tf.add(tf.matmul(recon, weights), forward_bias)
+    recon_hidden_prob = tf.divide(1.0, tf.add(1.0, tf.exp(tf.negative(recon_hidden_activation))))
+    if use_sampling:
+        recon_hidden = tf.to_float(tf.less(tf.random_uniform((1, num_hidden)), recon_hidden_prob))
+    else:
+        recon_hidden = recon_hidden_prob
+    print(sess.run(recon_hidden, {visible: x}))
