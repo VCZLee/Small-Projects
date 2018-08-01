fc4d8c34449143193c83572c7a36c05d49dbdfab
vczlee <vczlee@gmail.com>
2018-08-01 04:02:15 -0400

    fixed a bug where tensorflow was adding new nodes to the computational graph instead of only updating the weights and biases

--- Restricted Boltzmann Machine/Restricted Boltzmann Machine Tensorflow.py
+++ Restricted Boltzmann Machine/Restricted Boltzmann Machine Tensorflow.py
@@ -12,9 +12,9 @@ x = np.array([[1, 1, 1, 0, 1],
 
 num_visible = x.shape[1]
 num_hidden = 2
-use_sampling = True
+use_sampling = False
 learning_rate = 0.25
-epochs = 250
+epochs = 10000
 
 with tf.Session() as sess:
     weights = tf.Variable(tf.random_normal((num_visible, num_hidden)))
@@ -29,34 +29,34 @@ with tf.Session() as sess:
     else:
         hidden = hidden_prob
 
+    recon_activation = tf.add(tf.matmul(hidden, weights, transpose_b=True), backward_bias)
+    recon_prob = tf.divide(1.0, tf.add(1.0, tf.exp(tf.negative(recon_activation))))
+    if use_sampling:
+        recon = tf.to_float(tf.less(tf.random_uniform((1, num_visible)), recon_prob))
+    else:
+        recon = recon_prob
+
+    recon_hidden_activation = tf.add(tf.matmul(recon, weights), forward_bias)
+    recon_hidden_prob = tf.divide(1.0, tf.add(1.0, tf.exp(tf.negative(recon_hidden_activation))))
+    if use_sampling:
+        recon_hidden = tf.to_float(tf.less(tf.random_uniform((1, num_hidden)), recon_hidden_prob))
+    else:
+        recon_hidden = recon_hidden_prob
+
+    first_outer_product = tf.matmul(visible, hidden, transpose_a=True)
+    second_outer_product = tf.matmul(recon, recon_hidden, transpose_a=True)
+
+    contrastive_divergence = tf.divide(tf.subtract(first_outer_product, second_outer_product), x.shape[0])
+    forward_bias_update = tf.reduce_mean(tf.subtract(hidden, recon_hidden), 0, True)
+    backward_bias_update = tf.reduce_mean(tf.subtract(visible, recon), 0, True)
+
+    update_weights = tf.assign_add(weights, tf.multiply(learning_rate, contrastive_divergence))
+    update_forward_bias = tf.assign_add(forward_bias, tf.multiply(learning_rate, forward_bias_update))
+    update_backward_bias = tf.assign_add(backward_bias, tf.multiply(learning_rate, backward_bias_update))
+
     sess.run(tf.global_variables_initializer())
 
     for epoch in range(epochs):
-        recon_activation = tf.add(tf.matmul(hidden, weights, transpose_b=True), backward_bias)
-        recon_prob = tf.divide(1.0, tf.add(1.0, tf.exp(tf.negative(recon_activation))))
-        if use_sampling:
-            recon = tf.to_float(tf.less(tf.random_uniform((1, num_visible)), recon_prob))
-        else:
-            recon = recon_prob
-
-        recon_hidden_activation = tf.add(tf.matmul(recon, weights), forward_bias)
-        recon_hidden_prob = tf.divide(1.0, tf.add(1.0, tf.exp(tf.negative(recon_hidden_activation))))
-        if use_sampling:
-            recon_hidden = tf.to_float(tf.less(tf.random_uniform((1, num_hidden)), recon_hidden_prob))
-        else:
-            recon_hidden = recon_hidden_prob
-
-        first_outer_product = tf.matmul(visible, hidden, transpose_a=True)
-        second_outer_product = tf.matmul(recon, recon_hidden, transpose_a=True)
-
-        contrastive_divergence = tf.divide(tf.subtract(first_outer_product, second_outer_product), x.shape[0])
-        forward_bias_update = tf.reduce_mean(tf.subtract(hidden, recon_hidden), 0, True)
-        backward_bias_update = tf.reduce_mean(tf.subtract(visible, recon), 0, True)
-
-        update_weights = tf.assign_add(weights, tf.multiply(learning_rate, contrastive_divergence))
-        update_forward_bias = tf.assign_add(forward_bias, tf.multiply(learning_rate, forward_bias_update))
-        update_backward_bias = tf.assign_add(backward_bias, tf.multiply(learning_rate, backward_bias_update))
-
         sess.run((update_weights, update_forward_bias, update_backward_bias), {visible: x})
     print(sess.run(recon, {visible: x}))
 
